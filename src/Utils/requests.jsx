b1d6110cddfe38e8ea1101da2a76b39677a34dd4
Golodian <workuandebrhann05@gmail.com>
2025-02-14 15:10:50 -0500

    we have done Header,footer, Banner and Row

---
+++ src/Utils/requests.jsx
@@ -0,0 +1,44 @@
+const API_KEY = import.meta.env.VITE_REACT_API_KEY; 
+console.log("API Key:", API_KEY);
+
+const requests = { 
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`, 
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, 
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`, 
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`, 
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, 
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`, 
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`, 
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`, 
+    fetchTVShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`}; 
+    
+    export default requests;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const API_KEY = import.meta.env.VITE_TMDB_API_KEY; // Correct way to access API key in Vite
+
+// const requests = {
+//   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+//   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+//   fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+//   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+//   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+//   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+//   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+//   fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+//   fetchTVShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+// };
+
+// export default requests;
